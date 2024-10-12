3fef4713aef140aea3d8cfaaee43a6e658db750f
Kushagra9399 <146541055+Kushagra9399@users.noreply.github.com>
2024-10-12 22:13:57 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,7 +2,8 @@ import mysql.connector
 import Student
 import Queries
 
-# Establishing a database connection
+# Establishing a database connection2
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -29,15 +30,18 @@ def display_all(data=""):
         ins = query_all.select_all
         mycur.execute(ins)
         data = mycur.fetchall()
-    print("="*70)
-    print("| Id | Name           | Year | Mobile No   | Email")
+    name_len = email_len = 0
     for i in data:
-        print("| " + str(i[0]) + " "*(3-len(str(i[0]))),end="")
-        print("| " + i[1] + " "*(15-len(str(i[1]))),end="")
-        print("| " + str(i[2]) + " "*(5-len(str(i[2]))),end="")
-        print("| " + i[3] + " "*(12-len(str(i[3]))),end="")
-        print("| " + i[4] + " "*(30-len(str(i[4]))))
-    print("="*70)
+        name_len = max(name_len,len(i[1]))
+        email_len = max(email_len,len(i[-1]))
+    print("="*(40+name_len+email_len))
+    all_len = [3,name_len,5,12,email_len]
+    print("| Id  || Name" + " "*(name_len-4) + " || Year  || Mobile No    || Email" + " "*(email_len-5)+" |")
+    for i in data:
+        for j in range(len(i)):
+            print("| " + str(i[j]) + " "*(all_len[j]-len(str(i[j]))),end=" |")
+        print()
+    print("="*(40+email_len+name_len))
 
 def delete(a):
     col = ["id","Name","Year","mob","email"]
@@ -62,7 +66,12 @@ def update(a):
     print("Update data to: ")
     up = input()
     print("Update data by condition:\n1.Id\n2.Name\n3.Year\n4.Mobile No.\n5.Email\n6.Back")
-    i = int(input())
+    while True:
+        try:
+            i = int(input())
+            break
+        except Exception:
+            print("Enter Integer: ")
     if i in (1,2,3,4,5):
         j = input(f"{col_print[i-1]} equal to: ")
         ins = query_all.update_query(col[a-1],up,col[i-1],j)
@@ -72,8 +81,8 @@ def update(a):
 
 def search(a,b):
     col = ["id","Name","Year","mob","email"][a-1]
-    ins = f"SELECT * FROM trail WHERE {col} = %s;"
-    mycur.execute(ins,(b,))
+    ins = f"SELECT * FROM trail WHERE {col} LIKE %s;"
+    mycur.execute(ins,("%"+b+"%",))
     data = mycur.fetchall()
     display_all(data)
 
@@ -82,7 +91,12 @@ try:
         mycur.execute("set sql_safe_updates=0")
         while True:
             print("1.Add data\n2.Display all data\n3.Delete Data\n4.Update Data\n5.Search Data\n6.Self Query\n7.Exit")
-            c = int(input())
+            while True:
+                try:
+                    c = int(input())
+                    break
+                except Exception:
+                    print("Enter Integer: ")
             if c==1:
                 insert()
             elif c==2:
@@ -93,35 +107,69 @@ try:
             elif c==3:
                 print("Enter one data to delete row:")
                 print("1.Id\n2.Name\n3.Year\n4.Mobile No.\n5.Email\n6.Back")
-                dlt = int(input())
+                while True:
+                    try:
+                        dlt = int(input())
+                        if dlt not in [1,2,3,4,5,6]:
+                            print("Enter correct choice.\nRe-enter choice...")
+                            continue
+                        break
+                    except Exception:
+                        print("Enter Integer: ")
                 if dlt in [1,2,3,4,5]:
                     delete(dlt)
             elif c==4:
                 print("Update: \n1.Id\n2.Name\n3.Year\n4.Mobile No.\n5.Email\n6.Back")
-                sea = int(input())
-                if sea in [1,2,3,4,5]:
+                while True:
+                    try:
+                        sea = int(input())
+                        if sea in [1,2,3,4,5,6]:
+                            break
+                        else:
+                            print("Enter correct choice.\nRe-enter choice...")
+                    except Exception:
+                        print("Enter Integer: ")
+                if sea!=6:
                     update(sea)
             elif c==5:
                 print("Search by: \n1.Id\n2.Name\n3.Year\n4.Mobile No.\n5.Email\n6.Back")
-                sea = int(input())
-                if sea in [1,2,3,4,5]:
+                while True:
+                    try:
+                        sea = int(input())
+                        if sea in [1,2,3,4,5,6]:
+                            break
+                        else:
+                            print("Enter correct choice.\nRe-enter choice...")
+                    except Exception:
+                        print("Enter Integer: ")
+                while sea in [1,2,3,4,5]:
                     s1 = input("Enter data to search: ")
-                    search(sea,s1)
+                    if len(s1)<3:
+                        print("For search enter minimum 3 character!!!")
+                        continue
+                    else:
+                        break
+                search(sea,s1)
             elif c==6:
-                print("Write query: ")
-                ins = input()
-                a = ins.split()
-                if a[0]=="SELECT" or a[0]=="select":
-                    mycur.execute(ins)
-                    data = mycur.fetchall()
-                    for i in data:
-                        print(i)
-                else:
-                    mycur.execute(ins)
-                    mydb.commit()
-            else:
+                try:
+                    print("Write query: ")
+                    ins = input()
+                    a = ins.split()
+                    if a[0]=="SELECT" or a[0]=="select":
+                        mycur.execute(ins)
+                        data = mycur.fetchall()
+                        for i in data:
+                            print(i)
+                    else:
+                        mycur.execute(ins)
+                        mydb.commit()
+                except Exception:
+                    print("Invalid Query...")
+            elif c==7:
                 mycur.execute("set sql_safe_updates=1")
                 break
+            else:
+                print("Enter correct choice.\nRe-enter the choice...")
 
 except mysql.connector.Error as err:
     print(f"Error: {err}")
